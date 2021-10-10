7dc527a4433525177eacfeb6a46755fc4b24bd76
sergei <pozdeev.2002@inbox.ru>
2021-10-10 10:49:29 +0300

    Занятие 015, с помощью циклов и условий делали разные проверки, попробую залить этот проект на гит хаб. Вообще на самом деле прикольный язык программирования JS.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 "use strict";
+
+
+
+
+
 //1
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+
 
 
 //2
@@ -12,12 +19,30 @@ const personaMovieDB = {
     privat: false
 };
 
-const a =  prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++){
+    const a =  prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?','');
+    if (a != '' && b !='' && a != null && b != null && a.length < 50){
+        personaMovieDB.movies[a] = b;
+        console.log('done');
+    }else{
+        console.log('Error');
+        //Как вернуться на один цикл назад???
+        //Оператор инкремента
+        i--;
+    }
+}
+if(personaMovieDB.count < 10){
+    alert("Просмотрено довольно мало фмльмов!!!");
+}
+else if(10 <= personaMovieDB.count < 30){
+    alert("Вы класический зритель!");
+}
+else if(personaMovieDB.count > 30){
+    alert('Вы киноман!');
+}
+else{
+    alert('Произошла ошибка');
+}
 
-//Обращаемся к мувиес сначала пишем ключ, а потом значение.
-personaMovieDB.movies[a] = b;
-personaMovieDB.movies[c] = d;
 console.log(personaMovieDB);
